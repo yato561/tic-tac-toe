848c14e91cdc49d9c8b0fb31172774c79314e149
Shadow <manikantakommoju10@gmail.com>
2025-02-17 23:41:44 +0530

    Committing perfect number.

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 
 class PerfectNumber{
     public static boolean checknum(int num){
-            if(num<=1) return false;
+            if(num<=1) return false;// Ckecking whether num is lesser than or equal to 1 
             int sum=1;
             for(int i=2;i*i<=num;i++){
-                if(num%i==0){
+                if(num%i==0){//find the divisor of num
                     sum+=i;
-                    if(i!=num/i) sum+=num/i;
+                    if(i!=num/i) sum+=num/i;// finding out square root number so not to double count the num
                 }
             }
-            return sum==num;
+            return sum==num;// returing boolean value
         }
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
